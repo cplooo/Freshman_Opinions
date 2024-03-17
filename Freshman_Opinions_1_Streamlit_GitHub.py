21156370cc7d63abc1e3b66a9ddb6be9000f3ece
cplooo <cplo@kimo.com>
2024-03-17 17:56:29 +0800

    Add files via upload

--- Freshman_Opinions_1_Streamlit_GitHub.py
+++ Freshman_Opinions_1_Streamlit_GitHub.py
@@ -185,7 +185,7 @@ combined_df = pd.concat(dataframes, keys=selected_options)
 ####### 定義相關函數 (Part 2): 因為函數 'Draw' 的定義需要使用 'dataframes','combined_df' 來進行相關計算, 因此要放在以上 '預先設定' 之後才會有 'dataframes', 'combined_df' 的值
 ###### 畫圖形(單一學系或學院, 比較圖形)
 @st.cache_data(ttl=3600, show_spinner="正在處理資料...")  ## Add the caching decorator
-def Draw(院_系, column_index, split_symbol=';', dropped_string='沒有工讀', sum_choice=1, result_df=pd.DataFrame(), selected_options=[], dataframes=dataframes, combined_df=combined_df, width1=10,heigh1=6,width2=11,heigh2=8,width3=10,heigh3=6):
+def Draw(院_系, column_index, split_symbol=';', dropped_string='沒有工讀', sum_choice=1, result_df=pd.DataFrame(), selected_options=[], dataframes=dataframes, combined_df=combined_df, width1=10,heigh1=6,width2=11,heigh2=8,width3=10,heigh3=6,title_fontsize=15,xlabel_fontsize = 14,ylabel_fontsize = 14,legend_fontsize = 14):
     ##### 使用Streamlit畫單一圖
     if 院_系 == '0':
         collections = [df_freshman, df_freshman_faculty, df_freshman_original]
@@ -213,13 +213,13 @@ def Draw(院_系, column_index, split_symbol=';', dropped_string='沒有工讀',
         #### 设置y轴的位置
         r = np.arange(len(dataframes[0]))  ## len(result_df_理學_rr)=6, 因為result_df_理學_rr 有 6個 row: 非常滿意, 滿意, 普通, 不滿意, 非常不滿意
         #### 设置字体大小
-        title_fontsize = 15
-        xlabel_fontsize = 14
-        ylabel_fontsize = 14
+        title_fontsize = title_fontsize ##15
+        xlabel_fontsize = xlabel_fontsize  ##14
+        ylabel_fontsize = ylabel_fontsize  ##14
         xticklabel_fontsize = 14
         yticklabel_fontsize = 14
         annotation_fontsize = 8
-        legend_fontsize = 14
+        legend_fontsize = legend_fontsize  ##14
         #### 绘制条形
         fig, ax = plt.subplots(figsize=(width1, heigh1))
         # for i, (college_name, df) in enumerate(combined_df.groupby(level=0)):
@@ -556,7 +556,7 @@ with st.expander("Q9. 我的入學管道:"):
         ## 使用multiselect组件让用户进行多重选择
         selected_options = st.multiselect('選擇比較學院：', df_freshman_original['學院'].unique(), default=[choice,'資訊學院'],key=str(column_index)+'f')
 
-    Draw(院_系, column_index, ';', '沒有工讀', 1, result_df, selected_options, dataframes, combined_df,width1=15,heigh1=9,width2=15,heigh2=9,width3=15,heigh3=9)
+    Draw(院_系, column_index, ';', '沒有工讀', 1, result_df, selected_options, dataframes, combined_df,width1=15,heigh1=9,width2=15,heigh2=9,width3=15,heigh3=9,title_fontsize=17,xlabel_fontsize = 16,ylabel_fontsize = 16,legend_fontsize = 16)
     # Draw(院_系, column_index, split_symbol=';', dropped_string='沒有工讀', sum_choice=1, result_df, selected_options)
     
 st.markdown("##")  ## 更大的间隔   
