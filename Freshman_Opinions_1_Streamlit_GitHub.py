036b7d48b1c20ee0af7fd49b6ab3daffe965c47f
cplooo <cplo@kimo.com>
2024-03-17 21:20:06 +0800

    Add files via upload

--- Freshman_Opinions_1_Streamlit_GitHub.py
+++ Freshman_Opinions_1_Streamlit_GitHub.py
@@ -527,7 +527,7 @@ with st.expander("Q2. 身分別:"):
 
     # Draw(院_系, column_index, ';', '沒有工讀', 1, result_df, selected_options, dataframes, combined_df, bar_width = 0.15)
     # Draw(院_系, column_index, split_symbol=';', dropped_string='沒有工讀', sum_choice=1, result_df, selected_options)
-    Draw(院_系, column_index, split_symbol=';', dropped_string='沒有工讀', sum_choice=1, result_df=result_df, selected_options=selected_options, dataframes=dataframes, combined_df=combined_df, width1=10,heigh1=6,width2=11,heigh2=8,width3=10,heigh3=6,title_fontsize=15,xlabel_fontsize = 14,ylabel_fontsize = 14,legend_fontsize = 14,bar_width = 0.15)
+    Draw(院_系, column_index, split_symbol=';', dropped_string='沒有工讀', sum_choice=1, result_df=result_df, selected_options=selected_options, dataframes=dataframes, combined_df=combined_df, width1=10,heigh1=6,width2=11,heigh2=8,width3=10,heigh3=6,title_fontsize=15,xlabel_fontsize = 14,ylabel_fontsize = 14,legend_fontsize = 14,bar_width = 0.2)
     
 st.markdown("##")  ## 更大的间隔 
 
