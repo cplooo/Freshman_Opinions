09bc0e08c02cfda7a190315570806c2a33038a65
cplooo <cplo@kimo.com>
2024-06-24 03:54:01 +0800

    Add files via upload

--- Freshman_Opinions_1_Streamlit_GitHub.py
+++ Freshman_Opinions_1_Streamlit_GitHub.py
@@ -4447,30 +4447,6 @@ st.markdown("##")  ## 更大的间隔
 
 
 
-##### Q24.入學至今，對於「外語能力」的認同程度（範圍1～5；1為非常不瞭解；5為非常瞭解）: 分三群: 1+2,3,4+5
-with st.expander("Q24.入學至今，對於「外語能力」的認同程度各項目三等級呈現: 低(1+2),中(3),高(4+5):"):
-    df_freshman_r = df_freshman.iloc[:,list(range(94, 99))].reset_index(drop=True)
-    df_freshman_r.columns = [df_freshman_r.columns[i][4:] if i<9 else df_freshman_r.columns[i][5:] for i in range(df_freshman_r.shape[1])]
-    figure_title =choice+': '+'入學至今，對於「外語能力」的認同程度各項目三等級呈現: 低(1+2),中(3),高(4+5)'
-    # type(df_freshman_r.iloc[:,0][0])
-    
-    ####
-    df_freshman_r = df_freshman_r.applymap(lambda x: np.nan if x == '６不知道 ' else x)
-
-    #### 選擇性地，去掉包含 NaN 的行
-    df_freshman_r = df_freshman_r.dropna()
-
-    #### 轉換為int型態
-    for column in df_freshman_r.columns[0:df_freshman_r.shape[1]]:
-        df_freshman_r[column] = df_freshman_r[column].astype(int)
-    
-    
-    LevelGroupsDraw(df_freshman_r,level1=1,level2=2,level3=3,level4=4,level5=5,figure_title=figure_title,title_fontsize=13,xlabel_fontsize=12,ylabel_fontsize=12,yticklabel_fontsize=12,annotation_fontsize=12,legend_fontsize=12,width=10,height=6)
-
-st.markdown("##")  ## 更大的间隔
-
-
-
 st.markdown("""
 <style>
 .bold-small-font {
@@ -4678,6 +4654,31 @@ with st.expander("Q24-5.我認為自己在畢業後，能以外語與人有效�
     Draw(系_院_校, column_index, split_symbol=';', dropped_string='沒有工讀', sum_choice=1, result_df=result_df, selected_options=selected_options, dataframes=dataframes, combined_df=combined_df, width1=10,heigh1=6,width2=11,heigh2=8,width3=10,heigh3=6,title_fontsize=15,xlabel_fontsize = 14,ylabel_fontsize = 14,legend_fontsize = 14,xticklabel_fontsize = 14, yticklabel_fontsize = 14, annotation_fontsize = 14,bar_width = 0.2, fontsize_adjust=0)
     
 st.markdown("##")  ## 更大的间隔
+
+
+
+##### Q24.入學至今，對於「外語能力」的認同程度（範圍1～5；1為非常不瞭解；5為非常瞭解）: 分三群: 1+2,3,4+5
+with st.expander("Q24.入學至今，對於「外語能力」的認同程度各項目三等級呈現: 低(1+2),中(3),高(4+5):"):
+    df_freshman_r = df_freshman.iloc[:,list(range(94, 99))].reset_index(drop=True)
+    df_freshman_r.columns = [df_freshman_r.columns[i][4:] if i<9 else df_freshman_r.columns[i][5:] for i in range(df_freshman_r.shape[1])]
+    figure_title =choice+': '+'入學至今，對於「外語能力」的認同程度各項目三等級呈現: 低(1+2),中(3),高(4+5)'
+    # type(df_freshman_r.iloc[:,0][0])
+    
+    ####
+    df_freshman_r = df_freshman_r.applymap(lambda x: np.nan if x == '６不知道 ' else x)
+
+    #### 選擇性地，去掉包含 NaN 的行
+    df_freshman_r = df_freshman_r.dropna()
+
+    #### 轉換為int型態
+    for column in df_freshman_r.columns[0:df_freshman_r.shape[1]]:
+        df_freshman_r[column] = df_freshman_r[column].astype(int)
+    
+    
+    LevelGroupsDraw(df_freshman_r,level1=1,level2=2,level3=3,level4=4,level5=5,figure_title=figure_title,title_fontsize=13,xlabel_fontsize=12,ylabel_fontsize=12,yticklabel_fontsize=12,annotation_fontsize=12,legend_fontsize=12,width=10,height=6)
+
+st.markdown("##")  ## 更大的间隔
+
 
 
 st.markdown("""
