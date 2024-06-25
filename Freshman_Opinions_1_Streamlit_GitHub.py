54b52b11b8a8b47c82c040148ec837a32e1f549b
cplooo <cplo@kimo.com>
2024-06-25 15:48:20 +0800

    Add files via upload

--- Freshman_Opinions_1_Streamlit_GitHub.py
+++ Freshman_Opinions_1_Streamlit_GitHub.py
@@ -1041,7 +1041,7 @@ with st.expander("Q8. 學習及生活費（書籍、住宿、交通、伙食等�
     # Draw(系_院_校, column_index, ';', '沒有工讀', 1, result_df, selected_options, dataframes, combined_df)
     # Draw(系_院_校, column_index, split_symbol=';', dropped_string='沒有工讀', sum_choice=1, result_df, selected_options)
     Draw(系_院_校, column_index, split_symbol=';', dropped_string='沒有工讀', sum_choice=1, result_df=result_df, selected_options=selected_options, dataframes=dataframes, combined_df=combined_df, width1=10,heigh1=6,width2=11,heigh2=8,width3=10,heigh3=6,title_fontsize=15,xlabel_fontsize = 14,ylabel_fontsize = 14,legend_fontsize = 14,xticklabel_fontsize = 14, yticklabel_fontsize = 14, annotation_fontsize = 14,bar_width = 0.1, fontsize_adjust=0)    
-   plt.close()  # 關閉當前圖形以釋放內存
+    plt.close()  # 關閉當前圖形以釋放內存
 st.markdown("##")  ## 更大的间隔 
 
 
